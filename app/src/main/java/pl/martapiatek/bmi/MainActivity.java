82eb2d5c1399e925c3b5ea52b94cf074b5e4d1e5
Marta Piątek <martusia.piatek@gmail.com>
2018-03-22 17:46:59 +0100

    Obliczanie BMI

--- app/src/main/java/pl/martapiatek/bmi/MainActivity.java
+++ app/src/main/java/pl/martapiatek/bmi/MainActivity.java
@@ -1,5 +1,6 @@
 package pl.martapiatek.bmi;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +9,18 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editTextMass, editTextHeight;
+    private Button btnCount;
+    private TextView txtViewMass, txtViewHeigth, txtViewResult;
+
+    private BMICounter mBmiCounter;
+    double result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +29,53 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        editTextHeight = (EditText) findViewById(R.id.editTextHeight);
+        editTextMass = (EditText) findViewById(R.id.editTextMass);
+
+        txtViewHeigth = (TextView) findViewById(R.id.textViewHeight);
+        txtViewMass = (TextView) findViewById(R.id.textViewMass);
+        txtViewResult = (TextView) findViewById(R.id.textViewResult);
+
+        btnCount = (Button) findViewById(R.id.btnCount);
+
+
+        btnCount.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                double mass = 0 ;
+                double height = 0;
+                try {
+                    mass = Double.valueOf(editTextMass.getText().toString());
+                    height = Double.valueOf(editTextHeight.getText().toString());
+                } catch (Exception e) {
+                    e.printStackTrace();
+
+                }
+
+
+                mBmiCounter = new BMICounterForKgM(mass, height);
+
+                if (!mBmiCounter.isMassValid()) {
+                    editTextMass.setError(getString(R.string.err_wrongMass));
+                }
+                result = mBmiCounter.count();
+
+                txtViewResult.setText(String.valueOf(result));
+
+
             }
         });
+
+        txtViewResult.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this
+                        , BmiActivity.class);
+                i.putExtra("BMI", String.valueOf(result));
+                startActivity(i);
+            }
+        });
+
     }
 
     @Override
